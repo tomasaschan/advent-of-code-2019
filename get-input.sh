084252b09aaa9c211cf55510db2bf196af61ea1c
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-01 16:43:42 +0100

    Change default-location for input files

--- get-input.sh
+++ get-input.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-inputfile="./input/day$1.txt"
+inputfile="./input/dec$1.txt"
 
 if ! [ -f $inputfile ]; then
     curl -sSL https://adventofcode.com/2019/day/${1#0}/input --cookie session=$(<session.secret) -o $inputfile
