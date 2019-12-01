64c37066ec0d18e7137afc7fb2015921e0509911
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-01 16:35:33 +0100

    Add script for fetching input

---
+++ get-input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+inputfile="./input/day$1.txt"
+
+if ! [ -f $inputfile ]; then
+    curl -sSL https://adventofcode.com/2019/day/${1#0}/input --cookie session=$(<session.secret) -o $inputfile
+fi
