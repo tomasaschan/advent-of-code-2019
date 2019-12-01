fd6182663430b76cfb46284b8964680558871c8f
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-01 16:35:15 +0100

    Set up basic structure for AoC solving

---
+++ util/src/io.rs
@@ -0,0 +1,14 @@
+pub mod io {
+    use std::io;
+    use std::io::prelude::*;
+
+    pub fn get_input() -> String {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_to_string(&mut input)
+            .expect("Could not read input from stdin!");
+
+        return input;
+    }
+}
