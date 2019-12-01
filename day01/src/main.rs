0bbf8ff4cf118c3060bc0071cca387f2d21e0986
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-01 16:35:44 +0100

    Solve Dec 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,38 @@
+extern crate util;
+
+use util::io;
+
+fn fuel_for_mass(w: i32) -> i32 {
+    if w / 3 - 2 > 0 {
+        w / 3 - 2
+    } else {
+        0
+    }
+}
+
+fn rocket_eq_solution(w: i32) -> i32 {
+    let fuel = fuel_for_mass(w);
+    if fuel > 0 {
+        fuel + rocket_eq_solution(fuel)
+    } else {
+        fuel
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = io::get_input();
+
+    let a: i32 = input
+        .split_whitespace()
+        .map(|x| -> i32 { x.parse().expect("invalid input") })
+        .map(fuel_for_mass)
+        .sum();
+    println!("a: {}", a);
+
+    let b: i32 = input
+        .split_whitespace()
+        .map(|x| -> i32 { x.parse().expect("invalid input") })
+        .map(rocket_eq_solution)
+        .sum();
+    println!("b: {}", b);
 }
