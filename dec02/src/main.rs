574a854d5326708be1278bbd933a2c93fa13c1de
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-02 18:38:03 +0100

    Clean up a little, using itertools

--- dec02/src/main.rs
+++ dec02/src/main.rs
@@ -1,32 +1,16 @@
+extern crate itertools;
 extern crate util;
 
+use itertools::Itertools;
 use util::intcode_computer::run;
 use util::io;
 
 fn main() {
   let input = io::get_input();
-  let mut memory: Vec<u32> = parse(input);
+  let memory: Vec<u32> = parse(input);
 
-  memory[1] = 12;
-  memory[2] = 2;
-  println!("a: {}", run(memory.clone()));
-  let mut answer: i64 = -1;
-  for a in 0..99 {
-    for b in 0..99 {
-      memory[1] = a;
-      memory[2] = b;
-      let result = run(memory.clone());
-      if result == 19690720 {
-        answer = (100 * a + b) as i64;
-        break;
-      }
-    }
-    if answer > 0 {
-      break;
-    }
-  }
-
-  println!("b: {}", answer)
+  println!("a: {}", solve_a(memory.clone()));
+  println!("b: {}", solve_b(memory.clone()));
 }
 
 fn parse(input: String) -> Vec<u32> {
@@ -34,4 +18,22 @@ fn parse(input: String) -> Vec<u32> {
     .split(",")
     .map(|s| s.parse::<u32>().expect("invalid input"))
     .collect()
+}
+
+fn solve_a(mut memory: Vec<u32>) -> u32 {
+  memory[1] = 12;
+  memory[2] = 2;
+  return run(memory);
+}
+
+fn solve_b(mut memory: Vec<u32>) -> u32 {
+  for (a, b) in (0..99).cartesian_product(0..99) {
+    memory[1] = a;
+    memory[2] = b;
+    let result = run(memory.clone());
+    if result == 19690720 {
+      return 100 * a + b;
+    }
+  }
+  panic!("found no solution");
 }
