f4a290e24f821245f6b0602dd28a693098ce59cb
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-04 14:05:50 +0100

    Fix formatting

--- dec02/src/main.rs
+++ dec02/src/main.rs
@@ -6,34 +6,34 @@ use util::intcode_computer::run;
 use util::io;
 
 fn main() {
-  let input = io::get_input();
-  let memory: Vec<u32> = parse(input);
+    let input = io::get_input();
+    let memory: Vec<u32> = parse(input);
 
-  println!("a: {}", solve_a(memory.clone()));
-  println!("b: {}", solve_b(memory.clone()));
+    println!("a: {}", solve_a(memory.clone()));
+    println!("b: {}", solve_b(memory.clone()));
 }
 
 fn parse(input: String) -> Vec<u32> {
-  input
-    .split(",")
-    .map(|s| s.parse::<u32>().expect("invalid input"))
-    .collect()
+    input
+        .split(",")
+        .map(|s| s.parse::<u32>().expect("invalid input"))
+        .collect()
 }
 
 fn solve_a(mut memory: Vec<u32>) -> u32 {
-  memory[1] = 12;
-  memory[2] = 2;
-  return run(memory);
+    memory[1] = 12;
+    memory[2] = 2;
+    return run(memory);
 }
 
 fn solve_b(mut memory: Vec<u32>) -> u32 {
-  for (a, b) in (0..99).cartesian_product(0..99) {
-    memory[1] = a;
-    memory[2] = b;
-    let result = run(memory.clone());
-    if result == 19690720 {
-      return 100 * a + b;
+    for (a, b) in (0..99).cartesian_product(0..99) {
+        memory[1] = a;
+        memory[2] = b;
+        let result = run(memory.clone());
+        if result == 19690720 {
+            return 100 * a + b;
+        }
     }
-  }
-  panic!("found no solution");
+    panic!("found no solution");
 }
--- util/src/map_2d.rs
+++ util/src/map_2d.rs
@@ -2,43 +2,43 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct WorldMap<T> {
-  terrain: HashMap<(i32, i32), T>,
+    terrain: HashMap<(i32, i32), T>,
 }
 
 impl<T> WorldMap<T> {
-  pub fn new() -> WorldMap<T> {
-    WorldMap::<T> {
-      terrain: HashMap::new(),
+    pub fn new() -> WorldMap<T> {
+        WorldMap::<T> {
+            terrain: HashMap::new(),
+        }
     }
-  }
 
-  pub fn update<F: Fn(Option<&T>) -> T>(&mut self, coord: (i32, i32), f: F) {
-    let v = self.terrain.get(&coord);
-    let new_v = f(v);
-    self.terrain.insert(coord, new_v);
-  }
+    pub fn update<F: Fn(Option<&T>) -> T>(&mut self, coord: (i32, i32), f: F) {
+        let v = self.terrain.get(&coord);
+        let new_v = f(v);
+        self.terrain.insert(coord, new_v);
+    }
 }
 pub fn above(pos: (i32, i32)) -> (i32, i32) {
-  let (x, y) = pos;
-  (x, y + 1)
+    let (x, y) = pos;
+    (x, y + 1)
 }
 pub fn below(pos: (i32, i32)) -> (i32, i32) {
-  let (x, y) = pos;
-  (x, y - 1)
+    let (x, y) = pos;
+    (x, y - 1)
 }
 pub fn left_of(pos: (i32, i32)) -> (i32, i32) {
-  let (x, y) = pos;
-  (x - 1, y)
+    let (x, y) = pos;
+    (x - 1, y)
 }
 pub fn right_of(pos: (i32, i32)) -> (i32, i32) {
-  let (x, y) = pos;
-  (x + 1, y)
+    let (x, y) = pos;
+    (x + 1, y)
 }
 
 #[derive(Debug, Clone)]
 pub enum Direction {
-  Up,
-  Down,
-  Left,
-  Right,
+    Up,
+    Down,
+    Left,
+    Right,
 }
