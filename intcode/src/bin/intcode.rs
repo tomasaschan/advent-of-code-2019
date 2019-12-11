679f69fe4bc4fbef3597957c942b12ae36f49237
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-11 08:12:43 +0100

    Debugger work

--- intcode/src/bin/intcode.rs
+++ intcode/src/bin/intcode.rs
@@ -1,12 +1,20 @@
+use intcode::IntcodeComputer;
 use rustyline;
 use rustyline::error::ReadlineError;
+use std::collections::HashMap;
+use std::sync::mpsc::channel;
 
 pub fn main() {
     let mut rl = rustyline::Editor::<()>::new();
+    let (in_tx, in_rx) = channel();
+    let (out_tx, out_rx) = channel();
 
+    let mut computer = IntcodeComputer::new(&Vec::new(), None, None, in_rx, out_tx);
+
+    println!("Welcome to Intcode Debugger!");
     loop {
         match rl.readline("dbg> ") {
-            Ok(cmd) => println!("got {}", cmd),
+            Ok(cmd) => handle(cmd, &mut computer),
             Err(ReadlineError::Interrupted) => {
                 println!("^C");
                 break;
@@ -21,4 +29,12 @@ pub fn main() {
             }
         }
     }
+}
+
+fn handle(input: String, computer: &mut IntcodeComputer) {
+    if input.trim().len() == 0 {
+        return;
+    }
+    let cmd: Vec<&str> = input.split_whitespace().collect();
+    println!("{}", cmd[0]);
 }
