0bba4d5c73e255e5b081b41d890d8b88c43c053f
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-12-18 20:33:46 +0100

    Fix formatting

--- intcode/src/ops/mod.rs
+++ intcode/src/ops/mod.rs
@@ -1,142 +1,144 @@
 use super::{IntcodeComputer, Memory};
 
 impl IntcodeComputer {
-  pub fn step(&mut self) {
-    let op = self.memory.get(self.instruction_pointer) % 100;
-    let mode = self.memory.get(self.instruction_pointer) / 100;
-    match op {
-      1 => self.binary_op(|a, b| a + b, mode),
-      2 => self.binary_op(|a, b| a * b, mode),
-      3 => self.read(mode),
-      4 => self.write(mode),
-      5 => self.jmp_if(true, mode),
-      6 => self.jmp_if(false, mode),
-      7 => self.cmp(|a, b| a < b, mode),
-      8 => self.cmp(|a, b| a == b, mode),
-      9 => self.set_rel_base(mode),
-      _ => {
-        self.memory.dump();
-        panic!(
-          "invalid opcode {} at position {}. memory dumped to core.tmp",
-          op, self.instruction_pointer
-        )
-      }
-    };
-  }
-
-  fn binary_op<F: Fn(i128, i128) -> i128>(&mut self, op: F, mode: i128) {
-    let a = self.value_at_offset(1, param_mode(mode, 0));
-    let b = self.value_at_offset(2, param_mode(mode, 1));
-    self.set_at_offset(3, op(a, b), param_mode(mode, 2));
-    self.step_pointer(4);
-  }
-  fn jmp_if(&mut self, t: bool, mode: i128) {
-    let a = self.value_at_offset(1, param_mode(mode, 0));
-
-    if t ^ (a == 0) {
-      let p = self.value_at_offset(2, param_mode(mode, 1));
-      self.instruction_pointer = p;
-    } else {
-      self.step_pointer(3);
+    pub fn step(&mut self) {
+        let op = self.memory.get(self.instruction_pointer) % 100;
+        let mode = self.memory.get(self.instruction_pointer) / 100;
+        match op {
+            1 => self.binary_op(|a, b| a + b, mode),
+            2 => self.binary_op(|a, b| a * b, mode),
+            3 => self.read(mode),
+            4 => self.write(mode),
+            5 => self.jmp_if(true, mode),
+            6 => self.jmp_if(false, mode),
+            7 => self.cmp(|a, b| a < b, mode),
+            8 => self.cmp(|a, b| a == b, mode),
+            9 => self.set_rel_base(mode),
+            _ => {
+                self.memory.dump();
+                panic!(
+                    "invalid opcode {} at position {}. memory dumped to core.tmp",
+                    op, self.instruction_pointer
+                )
+            }
+        };
     }
-  }
 
-  fn cmp<F: Fn(i128, i128) -> bool>(&mut self, comparer: F, mode: i128) {
-    let a = self.value_at_offset(1, param_mode(mode, 0));
-    let b = self.value_at_offset(2, param_mode(mode, 1));
-
-    self.set_at_offset(3, if comparer(a, b) { 1 } else { 0 }, param_mode(mode, 2));
-    self.step_pointer(4);
-  }
-
-  fn set_rel_base(&mut self, mode: i128) {
-    let prev = self.memory.get(Memory::RELATIVE_BASE);
-    let diff = self.value_at_offset(1, param_mode(mode, 0));
-    self.memory.set(Memory::RELATIVE_BASE, prev + diff);
-    self.step_pointer(2);
-  }
-
-  fn write(&mut self, mode: i128) {
-    let v = self.value_at_offset(1, param_mode(mode, 0));
-    match self.output.send(v) {
-      Ok(_) => self.step_pointer(2),
-      Err(e) => panic!("Failure when writing to output: {:?}", e),
+    fn binary_op<F: Fn(i128, i128) -> i128>(&mut self, op: F, mode: i128) {
+        let a = self.value_at_offset(1, param_mode(mode, 0));
+        let b = self.value_at_offset(2, param_mode(mode, 1));
+        self.set_at_offset(3, op(a, b), param_mode(mode, 2));
+        self.step_pointer(4);
     }
-  }
-  pub fn value_at_offset(&self, offset: i128, mode: ParamMode) -> i128 {
-    let loc = match mode {
-      ParamMode::Position => self.memory.get(self.instruction_pointer + offset),
-      ParamMode::Immediate => self.instruction_pointer + offset,
-      ParamMode::Relative => {
-        self.memory.get(Memory::RELATIVE_BASE) + self.memory.get(self.instruction_pointer + offset)
-      }
-    };
-    self.memory.get(loc)
-  }
-  fn set_at_offset(&mut self, offset: i128, value: i128, mode: ParamMode) {
-    let loc = match mode {
-      ParamMode::Position => self.memory.get(self.instruction_pointer + offset),
-      ParamMode::Relative => {
-        self.memory.get(Memory::RELATIVE_BASE) + self.memory.get(self.instruction_pointer + offset)
-      }
-      ParamMode::Immediate => panic!(
-        "Tried to write in immediate mode! IP: {}",
-        self.instruction_pointer
-      ),
-    };
-    self.memory.set(loc, value);
-  }
+    fn jmp_if(&mut self, t: bool, mode: i128) {
+        let a = self.value_at_offset(1, param_mode(mode, 0));
 
-  fn step_pointer(&mut self, steps: i128) {
-    self.instruction_pointer = self.instruction_pointer + steps;
-  }
-  pub fn set_pointer(&mut self, loc: i128) {
-    self.instruction_pointer = loc;
-  }
+        if t ^ (a == 0) {
+            let p = self.value_at_offset(2, param_mode(mode, 1));
+            self.instruction_pointer = p;
+        } else {
+            self.step_pointer(3);
+        }
+    }
+
+    fn cmp<F: Fn(i128, i128) -> bool>(&mut self, comparer: F, mode: i128) {
+        let a = self.value_at_offset(1, param_mode(mode, 0));
+        let b = self.value_at_offset(2, param_mode(mode, 1));
+
+        self.set_at_offset(3, if comparer(a, b) { 1 } else { 0 }, param_mode(mode, 2));
+        self.step_pointer(4);
+    }
+
+    fn set_rel_base(&mut self, mode: i128) {
+        let prev = self.memory.get(Memory::RELATIVE_BASE);
+        let diff = self.value_at_offset(1, param_mode(mode, 0));
+        self.memory.set(Memory::RELATIVE_BASE, prev + diff);
+        self.step_pointer(2);
+    }
+
+    fn write(&mut self, mode: i128) {
+        let v = self.value_at_offset(1, param_mode(mode, 0));
+        match self.output.send(v) {
+            Ok(_) => self.step_pointer(2),
+            Err(e) => panic!("Failure when writing to output: {:?}", e),
+        }
+    }
+    pub fn value_at_offset(&self, offset: i128, mode: ParamMode) -> i128 {
+        let loc = match mode {
+            ParamMode::Position => self.memory.get(self.instruction_pointer + offset),
+            ParamMode::Immediate => self.instruction_pointer + offset,
+            ParamMode::Relative => {
+                self.memory.get(Memory::RELATIVE_BASE)
+                    + self.memory.get(self.instruction_pointer + offset)
+            }
+        };
+        self.memory.get(loc)
+    }
+    fn set_at_offset(&mut self, offset: i128, value: i128, mode: ParamMode) {
+        let loc = match mode {
+            ParamMode::Position => self.memory.get(self.instruction_pointer + offset),
+            ParamMode::Relative => {
+                self.memory.get(Memory::RELATIVE_BASE)
+                    + self.memory.get(self.instruction_pointer + offset)
+            }
+            ParamMode::Immediate => panic!(
+                "Tried to write in immediate mode! IP: {}",
+                self.instruction_pointer
+            ),
+        };
+        self.memory.set(loc, value);
+    }
+
+    fn step_pointer(&mut self, steps: i128) {
+        self.instruction_pointer = self.instruction_pointer + steps;
+    }
+    pub fn set_pointer(&mut self, loc: i128) {
+        self.instruction_pointer = loc;
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum ParamMode {
-  Immediate,
-  Position,
-  Relative,
+    Immediate,
+    Position,
+    Relative,
 }
 
 pub fn param_mode(mut mode: i128, mut n: i128) -> ParamMode {
-  loop {
-    if n == 0 {
-      break match mode % 10 {
-        0 => ParamMode::Position,
-        1 => ParamMode::Immediate,
-        2 => ParamMode::Relative,
-        _ => panic!("invalid parameter mode {}", mode),
-      };
+    loop {
+        if n == 0 {
+            break match mode % 10 {
+                0 => ParamMode::Position,
+                1 => ParamMode::Immediate,
+                2 => ParamMode::Relative,
+                _ => panic!("invalid parameter mode {}", mode),
+            };
+        }
+        mode /= 10;
+        n -= 1;
     }
-    mode /= 10;
-    n -= 1;
-  }
 }
 
 #[test]
 fn param_mode_test() {
-  assert_eq!(param_mode(0, 0), ParamMode::Position);
-  assert_eq!(param_mode(0, 1), ParamMode::Position);
-  assert_eq!(param_mode(0, 2), ParamMode::Position);
-  assert_eq!(param_mode(1, 0), ParamMode::Immediate);
-  assert_eq!(param_mode(1, 1), ParamMode::Position);
-  assert_eq!(param_mode(1, 2), ParamMode::Position);
-  assert_eq!(param_mode(10, 0), ParamMode::Position);
-  assert_eq!(param_mode(10, 1), ParamMode::Immediate);
-  assert_eq!(param_mode(10, 2), ParamMode::Position);
-  assert_eq!(param_mode(11, 0), ParamMode::Immediate);
-  assert_eq!(param_mode(11, 1), ParamMode::Immediate);
-  assert_eq!(param_mode(11, 2), ParamMode::Position);
-  assert_eq!(param_mode(110, 0), ParamMode::Position);
-  assert_eq!(param_mode(110, 1), ParamMode::Immediate);
-  assert_eq!(param_mode(110, 2), ParamMode::Immediate);
-  assert_eq!(param_mode(111, 0), ParamMode::Immediate);
-  assert_eq!(param_mode(111, 1), ParamMode::Immediate);
-  assert_eq!(param_mode(111, 2), ParamMode::Immediate);
+    assert_eq!(param_mode(0, 0), ParamMode::Position);
+    assert_eq!(param_mode(0, 1), ParamMode::Position);
+    assert_eq!(param_mode(0, 2), ParamMode::Position);
+    assert_eq!(param_mode(1, 0), ParamMode::Immediate);
+    assert_eq!(param_mode(1, 1), ParamMode::Position);
+    assert_eq!(param_mode(1, 2), ParamMode::Position);
+    assert_eq!(param_mode(10, 0), ParamMode::Position);
+    assert_eq!(param_mode(10, 1), ParamMode::Immediate);
+    assert_eq!(param_mode(10, 2), ParamMode::Position);
+    assert_eq!(param_mode(11, 0), ParamMode::Immediate);
+    assert_eq!(param_mode(11, 1), ParamMode::Immediate);
+    assert_eq!(param_mode(11, 2), ParamMode::Position);
+    assert_eq!(param_mode(110, 0), ParamMode::Position);
+    assert_eq!(param_mode(110, 1), ParamMode::Immediate);
+    assert_eq!(param_mode(110, 2), ParamMode::Immediate);
+    assert_eq!(param_mode(111, 0), ParamMode::Immediate);
+    assert_eq!(param_mode(111, 1), ParamMode::Immediate);
+    assert_eq!(param_mode(111, 2), ParamMode::Immediate);
 }
 
 pub mod read;
--- intcode/tests/intcode.rs
+++ intcode/tests/intcode.rs
@@ -2,75 +2,75 @@ use intcode::*;
 use std::iter::{empty, once, repeat};
 
 fn get_value_at_0(program: &Vec<i128>, input: &mut dyn Iterator<Item = i128>) -> i128 {
-  let output = Builder::new()
-    .program(&program)
-    .exit_hook(vec![4, 0, 99])
-    .run_noninteractive(input);
-  *output.last().unwrap()
+    let output = Builder::new()
+        .program(&program)
+        .exit_hook(vec![4, 0, 99])
+        .run_noninteractive(input);
+    *output.last().unwrap()
 }
 fn get_last_output(program: &Vec<i128>, input: &mut dyn Iterator<Item = i128>) -> i128 {
-  let output = Builder::new().program(&program).run_noninteractive(input);
-  *output.last().unwrap()
+    let output = Builder::new().program(&program).run_noninteractive(input);
+    *output.last().unwrap()
 }
 
 #[test]
 fn addmul_tests() {
-  let output1 = get_value_at_0(
-    &vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
-    &mut empty(),
-  );
-  assert_eq!(output1, 3500);
+    let output1 = get_value_at_0(
+        &vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+        &mut empty(),
+    );
+    assert_eq!(output1, 3500);
 
-  let output2 = get_value_at_0(&vec![1, 0, 0, 0, 99], &mut empty());
-  assert_eq!(output2, 2);
+    let output2 = get_value_at_0(&vec![1, 0, 0, 0, 99], &mut empty());
+    assert_eq!(output2, 2);
 
-  let output3 = get_value_at_0(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99], &mut empty());
-  assert_eq!(output3, 30);
+    let output3 = get_value_at_0(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99], &mut empty());
+    assert_eq!(output3, 30);
 }
 
 #[test]
 fn jmp_tests() {
-  let out1 = get_last_output(
-    &vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
-    &mut once(0),
-  );
-  assert_eq!(out1, 0);
+    let out1 = get_last_output(
+        &vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
+        &mut once(0),
+    );
+    assert_eq!(out1, 0);
 
-  let out2 = get_last_output(
-    &vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1],
-    &mut once(1),
-  );
-  assert_eq!(out2, 1);
+    let out2 = get_last_output(
+        &vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1],
+        &mut once(1),
+    );
+    assert_eq!(out2, 1);
 }
 
 #[test]
 fn cmp_tests() {
-  let mut sevens = repeat(7);
-  let mut eights = repeat(8);
+    let mut sevens = repeat(7);
+    let mut eights = repeat(8);
 
-  for program in [
-    vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8],
-    vec![3, 3, 1108, -1, 8, 3, 4, 3, 99],
-  ]
-  .iter()
-  {
-    let out1 = get_last_output(&program, &mut eights);
-    assert_eq!(out1, 1);
+    for program in [
+        vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8],
+        vec![3, 3, 1108, -1, 8, 3, 4, 3, 99],
+    ]
+    .iter()
+    {
+        let out1 = get_last_output(&program, &mut eights);
+        assert_eq!(out1, 1);
 
-    let out2 = get_last_output(&program, &mut sevens);
-    assert_eq!(out2, 0);
-  }
+        let out2 = get_last_output(&program, &mut sevens);
+        assert_eq!(out2, 0);
+    }
 
-  for program in [
-    vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8],
-    vec![3, 3, 1107, -1, 8, 3, 4, 3, 99],
-  ]
-  .iter()
-  {
-    let out1 = get_last_output(&program, &mut eights);
-    assert_eq!(out1, 0);
+    for program in [
+        vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8],
+        vec![3, 3, 1107, -1, 8, 3, 4, 3, 99],
+    ]
+    .iter()
+    {
+        let out1 = get_last_output(&program, &mut eights);
+        assert_eq!(out1, 0);
 
-    let out2 = get_last_output(&program, &mut sevens);
-    assert_eq!(out2, 1);
-  }
+        let out2 = get_last_output(&program, &mut sevens);
+        assert_eq!(out2, 1);
+    }
 }
