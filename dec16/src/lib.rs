31517d62e409e2e3806535af9c7f56e7daeeb5b8
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-05-11 23:19:42 +0200

    Start working on dec 16

---
+++ dec16/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn solve_a(input: &String) -> i128 {
+    println!("{:?}", input.split("").map(|c| c.parse::<usize>()));
+    0
+}
---
+++ dec16/tests/dec16.rs
@@ -0,0 +1,16 @@
+use aoc_2019_16::*;
+use util::io::read_file;
+
+#[test]
+fn solves_a() {
+    let input = read_file("../input/dec16.txt");
+
+    assert_eq!(solve_a(&input), 0);
+}
+
+//#[test]
+//fn solves_b() {
+//    let input = read_file("../input/dec16.txt");
+//
+//    assert_eq!(solve_b(&input), ...);
+//}
